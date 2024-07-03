cfcb5c75d1ed94792aa5157a858b8354293821ee
Aman Saini <aman.singh756888@gmail.com>
2024-07-03 21:13:44 +0530

    Mongoosse complete

--- Node/server.js
+++ Node/server.js
@@ -25,12 +25,19 @@ const userSchema = new mongoose.Schema({
     },
     age : {
         type:Number,
-        min:18
+        min:18,
+        validate:{
+            validator: (v)=>{
+                return v % 2 === 0;
+            },
+            msg:"provide even age"
+        }
     },
     email :{
         type:String,
         required : true,
-        toLowerCase:true,
+        lowercase:true,
+        unique:true,
     },
     createdAt:{
         type : Date,
@@ -42,6 +49,7 @@ const userSchema = new mongoose.Schema({
     },
     bestfriend:{
         type: mongoose.SchemaTypes.ObjectId,
+        ref:"users"
     },
     hobbies: {
         type:[String],
@@ -53,6 +61,21 @@ const userSchema = new mongoose.Schema({
         }
     }
     
+});
+
+userSchema.methods.sayHi = function(){
+    console.log(`hello i am ${this.Name}`);
+}
+
+userSchema.pre('save',function(next){
+    this.updatedAt = Date.now();
+    next();
+})
+
+// MiddleWare
+userSchema.post('save',function(doc,next){
+    doc.sayHi();
+    next();
 })
 
 // model schema => apply this schema to mongo collection
@@ -60,10 +83,13 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('users',userSchema);
 
 const getAllUsers = async()=>{
-    const allUsers = await User.find();
-    console.log(allUsers);
+    const allUsers = await User.find().populate('bestfriend');
+    // console.log(allUsers);
+    allUsers.forEach(user=>{
+        user.sayHi();
+    })
 }
-getAllUsers();
+// getAllUsers();
 
 const insertNewUser = async(newUser)=>{
     const user = await User.create(newUser);
@@ -71,7 +97,7 @@ const insertNewUser = async(newUser)=>{
 
 }
 
-// insertNewUser({Name:"Aman",age:19});
+// insertNewUser({Name:"julie",age:18,email:"julie@abc.com"});
 
 const updateUser = async()=>{
     // const myUsers = await User.find({Name:"Aman"});
@@ -79,11 +105,12 @@ const updateUser = async()=>{
     // myUsers[0].Name = "Aman Saini";
     // myUsers[0].save()
 
-    const myUser = await User.findOne({Name:"Aman"});
+    const myUser = await User.findOne({Name:"julie"});
     // myUser.Name = "Aman Saini";
     // myUser.age = 19;
     // myUser.gender = "Male";
-    myUser.email = "Aman@abc.com";
+    // myUser.email = "Aman@abc.com";
+    myUser.bestfriend = "668566279c875ef9e4e9778e";
     await myUser.save();
     console.log(myUser);
 
