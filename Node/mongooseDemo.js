208dba6b8a22e048a5a3ab2e8fac293fe2e95646
Aman Saini <aman.singh756888@gmail.com>
2024-07-08 22:27:04 +0530

    express Demo

---
+++ Node/mongooseDemo.js
@@ -0,0 +1,142 @@
+// MVC => Model Routes Control 
+
+console.log("mongoose");
+
+// import
+const mongoose =  require("mongoose");
+
+const connectToDb = async (URI)=>{
+    try{
+        await mongoose.connect(URI);
+        console.log("connected to DB");
+    }
+    catch(err){
+        console.log(err);
+    }
+    
+}
+
+connectToDb("mongodb://127.0.0.1:27017/school");
+console.log("I am here");
+
+// schema => we define structure
+
+const userSchema = new mongoose.Schema({
+    Name: {
+        type:String
+    },
+    age : {
+        type:Number,
+        min:18,
+        validate:{
+            validator: (v)=>{
+                return v % 2 === 0;
+            },
+            msg:"provide even age"
+        }
+    },
+    email :{
+        type:String,
+        required : true,
+        lowercase:true,
+        unique:true,
+    },
+    createdAt:{
+        type : Date,
+        default: ()=> Date.now()
+    },
+    updatedAt:{
+        type : Date,
+        default: ()=> Date.now()
+    },
+    bestfriend:{
+        type: mongoose.SchemaTypes.ObjectId,
+        ref:"users"
+    },
+    hobbies: {
+        type:[String],
+    },
+    address: {
+        type : {
+            City:String,
+            pin: Number
+        }
+    }
+    
+});
+
+userSchema.methods.sayHi = function(){
+    console.log(`hello i am ${this.Name}`);
+}
+
+userSchema.pre('save',function(next){
+    this.updatedAt = Date.now();
+    next();
+})
+
+userSchema.pre("deleteOne",function(next){
+    console.log(this.Name,"is getting deleted");
+    next();
+})
+// MiddleWare
+userSchema.post('save',function(doc,next){
+    doc.sayHi();
+    next();
+})
+
+// model schema => apply this schema to mongo collection
+
+const User = mongoose.model('users',userSchema);
+
+const getAllUsers = async()=>{
+    const allUsers = await User.find().populate('bestfriend');
+    // console.log(allUsers);
+    allUsers.forEach(user=>{
+        user.sayHi();
+    })
+}
+// getAllUsers();
+
+const insertNewUser = async(newUser)=>{
+    const user = await User.create(newUser);
+    console.log(user);
+
+}
+const opts = { runValidators: true };
+const insertMultiUser = async(AllNewUsers)=>{
+
+    const users = await User.insertMany(AllNewUsers,opts);
+    console.log(users);
+
+}
+
+// insertNewUser({Name:"julie",age:18,email:"julie@abc.com"});
+
+// insertMultiUser([{Name:"jim",age:20,email:"jim@abc.com"},{Name:"ram",age:18,email:"ram@abc.com"},{Name:"shiva",age:24,email:"shiva@abc.com"}])
+
+const deleteUser = async (deleteFilter)=>{
+    const res = await User.deleteOne(deleteFilter,opts);
+
+    console.log(res);
+}
+deleteUser({Name:"ram"});
+const updateUser = async()=>{
+    // const myUsers = await User.find({Name:"Aman"});
+    // console.log(myUsers);
+    // myUsers[0].Name = "Aman Saini";
+    // myUsers[0].save()
+
+    const myUser = await User.findOne({Name:"julie"});
+    // myUser.Name = "Aman Saini";
+    // myUser.age = 19;
+    // myUser.gender = "Male";
+    // myUser.email = "Aman@abc.com";
+    myUser.bestfriend = "668566279c875ef9e4e9778e";
+    await myUser.save();
+    console.log(myUser);
+
+    // const myUser = await User.findOneAndUpdate({Name:"Aman Saini"},{gender:"Male"});
+    // console.log(myUser);
+}
+
+// updateUser();
--- Node/server.js
+++ Node/server.js
@@ -1,121 +1,39 @@
-console.log("mongoose");
+// express
 
-// import
-const mongoose =  require("mongoose");
+// console.log("express");
 
-const connectToDb = async (URI)=>{
-    try{
-        await mongoose.connect(URI);
-        console.log("connected to DB");
-    }
-    catch(err){
-        console.log(err);
-    }
-    
-}
+const express = require("express");
 
-connectToDb("mongodb://127.0.0.1:27017/school");
-console.log("I am here");
+const app = express();
 
-// schema => we define structure
+// get, post, put, patch, delete, listen, use
 
-const userSchema = new mongoose.Schema({
-    Name: {
-        type:String
-    },
-    age : {
-        type:Number,
-        min:18,
-        validate:{
-            validator: (v)=>{
-                return v % 2 === 0;
-            },
-            msg:"provide even age"
-        }
-    },
-    email :{
-        type:String,
-        required : true,
-        lowercase:true,
-        unique:true,
-    },
-    createdAt:{
-        type : Date,
-        default: ()=> Date.now()
-    },
-    updatedAt:{
-        type : Date,
-        default: ()=> Date.now()
-    },
-    bestfriend:{
-        type: mongoose.SchemaTypes.ObjectId,
-        ref:"users"
-    },
-    hobbies: {
-        type:[String],
-    },
-    address: {
-        type : {
-            City:String,
-            pin: Number
-        }
-    }
-    
-});
+// 3000 9999
 
-userSchema.methods.sayHi = function(){
-    console.log(`hello i am ${this.Name}`);
-}
+// app.get("/",(req,res)=>{
+//     res.status(200).setHeader("content-type","text/plain").send("Hello Friends");
+// })
+// app.get("/user",(req,res)=>{
+//     res.setHeader("content-type","application/json");
+//     res.json({"Name":"Aman Saini","Age":29});
+// })
 
-userSchema.pre('save',function(next){
-    this.updatedAt = Date.now();
-    next();
+// app.get("/aman",(req,res)=>{
+//     res.sendFile("./demo.txt",{root:"./"});
+// })
+
+app.use("/",express.static("./staticApp/public"));
+
+app.get("/",(req,res)=>{
+    res.status(200).sendFile("./staticApp/index.html",{root:"./"});
 })
+// app.get("/common.css",(req,res)=>{
+//     res.status(200).sendFile("./staticApp/common.css",{root:"./"});
+// })
 
-// MiddleWare
-userSchema.post('save',function(doc,next){
-    doc.sayHi();
-    next();
+// app.get("/index.js",(req,res)=>{
+//     res.status(200).sendFile("./staticApp/index.js",{root:"./"});
+// })
+app.listen(8000,()=>{
+    console.log("listening on port 8000");
 })
-
-// model schema => apply this schema to mongo collection
-
-const User = mongoose.model('users',userSchema);
-
-const getAllUsers = async()=>{
-    const allUsers = await User.find().populate('bestfriend');
-    // console.log(allUsers);
-    allUsers.forEach(user=>{
-        user.sayHi();
-    })
-}
-// getAllUsers();
-
-const insertNewUser = async(newUser)=>{
-    const user = await User.create(newUser);
-    console.log(user);
-
-}
-
-// insertNewUser({Name:"julie",age:18,email:"julie@abc.com"});
-
-const updateUser = async()=>{
-    // const myUsers = await User.find({Name:"Aman"});
-    // console.log(myUsers);
-    // myUsers[0].Name = "Aman Saini";
-    // myUsers[0].save()
-
-    const myUser = await User.findOne({Name:"julie"});
-    // myUser.Name = "Aman Saini";
-    // myUser.age = 19;
-    // myUser.gender = "Male";
-    // myUser.email = "Aman@abc.com";
-    myUser.bestfriend = "668566279c875ef9e4e9778e";
-    await myUser.save();
-    console.log(myUser);
-
-    // const myUser = await User.findOneAndUpdate({Name:"Aman Saini"},{gender:"Male"});
-    // console.log(myUser);
-}
-
-updateUser();
