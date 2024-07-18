e838fd7f7cb825cadab0cc42c04f05f8d66f88e4
Aman Saini <aman.singh756888@gmail.com>
2024-07-18 20:58:15 +0530

    till adding data to db using server

--- Node/customer.controller.js
+++ Node/customer.controller.js
@@ -1,10 +1,50 @@
 const mongoose = require("mongoose");
 
 const Customer = require("./customer.model")(mongoose);
-
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+function validatePhoneNumber(input_str) {
+    var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    
+    return re.test(input_str);
+    }
 const addNewCustomer = async(req,res)=>{
-    const newCustomer = req.body;
+    // const newCustomer = req.body;
+    const {firstName,lastName,email,mobile,address} = req.body;
+    const newCustomer = {};
     // validations
+    if(firstName && firstName.trim().length >0){
+        newCustomer.firstName = firstName.trim();
+    }
+    else{
+        res.status(400).send({success:false,msg:"first name field is required"});
+        return;
+    }
+    if(lastName && lastName.trim().length >0){
+        newCustomer.lastName = lastName.trim();
+    }
+    else{
+        res.status(400).send({success:false,msg:"last name field is required"});
+        return;
+    }
+    if(email && email.trim().length >0 && validateEmail(email.trim())){
+        newCustomer.email = email.trim();
+    }
+    else{
+        res.status(400).send({success:false,msg:"enter valid email required"});
+        return;
+    }
+    
+    if(mobile && mobile.trim().length === 10 && validatePhoneNumber(mobile)){
+
+        newCustomer.mobile = mobile.trim();
+    }
+    else if (mobile.trim()){
+        res.status(400).send({success:false,msg:"enter valid mobile no."});
+        return;
+    }
     try {
         const resp = await Customer.create(newCustomer);
         res.status(201).send({success:true,msg:"customer created",data:resp});
@@ -13,6 +53,7 @@ const addNewCustomer = async(req,res)=>{
 
         console.log(error);
         res.status(400).send({success:false,msg:"some error occured",err:error.toString()});
+        return
     }
 }
 
