f015291747e9c479ef23519fede4bce304ab09a1
Aman Saini <aman.singh756888@gmail.com>
2024-08-12 21:05:08 +0530

    revision conditional if

--- Js/RevesionDataTypes.js
+++ Js/RevesionDataTypes.js
@@ -52,10 +52,10 @@ console.log(x);
 
 // delete
 
-console.log(x.pop(),x);
+// console.log(x.pop(),x);
 
 // update
-x[0] = 15;
+x[0] = 15; // [15,13,14]
 console.log(x[1]);
 
 // search / vist
@@ -68,16 +68,95 @@ for (let i = 0 ; i < x.length ; i++) {
 
 }
 
+// forEach, map, filter
+
+const myFunc = (element)=>{
+    // any logic
+    element = element + 1;
+    console.log("ele",element);
+    // return element;
+}
+// does not get anything in return 
+// no change in values of array
+let z = x.forEach(myFunc);
+
+console.log(z,"z");
+
+console.log(x);
+
+// map 
+
+
+z = x.map(myFunc);
+
+
+console.log(z,"z");
+
+console.log(x);
+
+const MyFilter = (element)=>{
+    if (element % 2 === 0){
+        return true;
+    }
+    return false;
+
+}
+
+// filter
+z = x.filter(MyFilter)
+
+console.log(z,"filter");
+
+console.log(x)
 
 
 
+// Objects
+
+// create object
+// x = {
+//     name:"Aman"
+// }
+
+x = {};
+
+// add new Properties to obj
+
+x.email  = "aman@abc.com"
 
 
+x.firstName = "Aman Saini"
 
+x.hoobies = ["dancing","singing"];
 
+x.address = {
+    city:"mohali"
+}
 
+// update
 
+x.email = "aman@xyz.com";
 
+console.log(x);
 
+// delete 
+
+delete x.email;
+
+console.log(x);
+
+// access
+
+console.log(x.firstName);
+
+console.log(x['address']);
+
+// traverse
+
+for( i in x){
+    // console.log(x.i);
+    console.log(x[i]);
+   
+}
 
 
---
+++ Js/RevisionConditional.js
@@ -0,0 +1,35 @@
+console.log("conditional");
+// Decision making 
+// to make any decision
+
+// if conditional 
+// switch case
+
+//  if(conditions) {
+//    task
+// }
+// else{
+// 
+// }
+
+
+let age = 17;
+
+if(age >= 18){
+    console.log("eligible for dl");
+  
+}
+else if( age >= 17){
+    console.log("elegible for learning");
+    
+}
+
+else{
+    console.log("not elegible");
+    
+}
+
+
+// if (condition) {
+    
+// }
